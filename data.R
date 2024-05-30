b4033a8b63783694125e7204fcff608759bef5f7
Simon Fischer <32540427+shfischer@users.noreply.github.com>
2024-05-30 20:58:37 +0100

    remove redundant scripts

--- data.R
+++ data.R
@@ -10,24 +10,11 @@ library(icesTAF)
 ### create folder to store data
 mkdir("data")
 
-### extract data from InterCatch
-sourceTAF("data_1_InterCatch_extract.R")
+### catch data
+sourceTAF("data_catch.R")
 
-### processs data from InterCatch and add to time series (VPA files)
-sourceTAF("data_2_migration_weights.R")
+### survey data
+sourceTAF("data_surveys.R")
 
-### extend tuning time series
-sourceTAF("data_3_tuning.R")
-
-### load data required for stock assessment
-sourceTAF("data_4_assessment.R")
-
-### length data
-sourceTAF("data_5_length_data.R")
-
-### prepare discard data
-sourceTAF("data_6_discards.R")
-
-### prepare data for rfb rule
-# sourceTAF("data_7_rfb.R")
-
+### ALKs
+sourceTAF("data_ALK.R")
