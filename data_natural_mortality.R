fe789b6855f56683f5644cbad61b4198efd0ac3b
Simon Fischer <32540427+shfischer@users.noreply.github.com>
2024-05-30 21:20:14 +0100

    natural mortality

---
+++ data_natural_mortality.R
@@ -0,0 +1,68 @@
+### ------------------------------------------------------------------------ ###
+### natural mortality M ####
+### ------------------------------------------------------------------------ ###
+
+mkdir("data/M")
+mkdir("data/M/plots")
+
+library(icesTAF)
+taf.libPaths()
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+if (!exists("verbose")) verbose <- FALSE
+
+### ------------------------------------------------------------------------ ###
+### M at age estimators ####
+### ------------------------------------------------------------------------ ###
+
+### get von Bertalanffy growth parameters
+vB_pars <- read.csv("data/ALKs/vB_pars.csv")
+### use values estimated with last five years of data
+vB_Linf <- tail(vB_pars$Linf, 1)
+vB_k <- tail(vB_pars$k, 1)
+vB_t0 <- tail(vB_pars$t0, 1)
+
+### von Bertalanffy growth function
+vB_length <- function(L_inf, k, t, t_0 = 0){
+  L_inf * (1 - exp(-k * (t - t_0)))
+}
+
+### Gislason et al. (2010)
+### https://doi.org/10.1111/j.1467-2979.2009.00350.x
+M_Gislason <- function(L, Linf, k) {
+  exp(0.55 - 1.61*log(L) + 1.44*log(Linf) + log(k))
+}
+
+### Lorenzen et al. (2022)
+### https://doi.org/10.1016/j.fishres.2022.106327
+M_Lorenzen <- function(L, Linf, k) {
+  exp(0.28 - 1.30*log(L/Linf) + 1.08*log(k))
+}
+
+### table with Ms
+ages <- 1:15
+lengths <- vB_length(L_inf = vB_Linf, k = vB_k, t_0 = vB_t0, t = ages)
+df_M <- data.frame(
+  age = ages,
+  M_default = 0.12,
+  M_Gislason = M_Gislason(L = lengths, Linf = vB_Linf, k = vB_k),
+  M_Lorenzen = M_Lorenzen(L = lengths, Linf = vB_Linf, k = vB_k)
+  )
+p <- df_M %>%
+  pivot_longer(-1) %>%
+  mutate(name = gsub(x = name, pattern = "M_", replacement = "")) %>%
+  mutate(name = factor(name, levels = c("default", "Gislason", "Lorenzen"))) %>%
+  ggplot(aes(x = age, y = value, colour = name)) +
+  geom_line() +
+  scale_colour_discrete("") + 
+  xlim(c(0, NA)) +
+  ylim(c(0, NA)) + 
+  labs(x = "Age (years)", y = "Natural mortality M (per year)") +
+  theme_bw(base_size = 8) +
+  theme(legend.key.height = unit(0.5, "lines"))
+if (isTRUE(verbose)) p
+ggsave("data/M/plots/M_options.png", 
+       width = 15, height = 10, units = "cm", dpi = 300, plot = p)
+
